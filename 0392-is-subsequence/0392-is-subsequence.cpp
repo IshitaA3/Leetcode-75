2e2066f5303ff4099faddb4614b0c903703d41c1
IshitaA3 <agishita13@gmail.com>
2025-08-03 22:27:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (89.93%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        unordered_map<char, int>mp;
-        for(int i=0;i<t.size();i++) {
-            if(mp.find(t[i]) == mp.end()) mp[t[i]] = i;
-        }
-        int prev = -1;
-        // for(int i=0;i<s.size();i++){
-        //     if(mp.find(s[i]) != mp.end()){
-        //       if(mp[s[i]] < prev) return 0;
-        //       else prev= mp[s[i]];      
-        //     }
-        //     else return 0;
-        // }
-        // return 1;
         int i = 0, j=0;
         while(i<s.size() and j<t.size()){
             while(j<t.size() and t[j] != s[i])j++;
